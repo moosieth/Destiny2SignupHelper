383b975802d7db396b5cf4da451cfefb64420b40
Mitchell Frazer <108438939+moosieth@users.noreply.github.com>
2024-10-21 20:54:56 -0600

    refactor: use non-dynamic buttons for rsvp functions

--- D2SignupHelper/d2signuphelper/views/listing_view.py
+++ D2SignupHelper/d2signuphelper/views/listing_view.py
@@ -13,55 +13,24 @@ from d2signuphelper.mongo_interaction.listings import (
 )
 
 
-class RSVPButton(
-    discord.ui.DynamicItem[discord.ui.Button],
-    template=r"rsvp:user:(?P<id>[0-9]+):mongoid:(?P<mid>[a-fA-F0-9]+):max:(?P<max>[0-9]+):cur:(?P<cur>[0-9]+)",
-):
-    def __init__(
-        self, user_id: int, listing_mongo_id: str, max: int, cur: int = 0
-    ) -> None:
-        self.user_id: int = user_id
-        self.max: int = max
-        self.cur = cur
+class RSVPButton(Button):
+    def __init__(self, listing_mongo_id: str, max: int, creator: int):
         self.mongo_id = listing_mongo_id
+        self.max = max
+        self.cur = 0
+        self.creator = creator
         super().__init__(
-            discord.ui.Button(
-                label="Count me in!",
-                style=self.style,
-                custom_id=f"rsvp:user:{user_id}:mongoid:{listing_mongo_id}:max:{max}:cur:{cur}",
-                emoji="\N{THUMBS UP SIGN}",
-            )
+            label="Count me in!",
+            style=discord.ButtonStyle.grey,
+            emoji="\N{THUMBS UP SIGN}",
         )
 
-    @property
-    def style(self) -> discord.ButtonStyle:
-        if self.cur < self.max:
-            return discord.ButtonStyle.grey
-        self.disabled = True
-        return discord.ButtonStyle.red
-
-    # This method actually extracts the information from the custom ID and creates the item.
-    @classmethod
-    async def from_custom_id(
-        cls,
-        interaction: discord.Interaction,
-        item: discord.ui.Button,
-        match: re.Match[str],
-        /,
-    ):
-        mongo_id = str(match["mid"])
-        user_id = int(match["id"])
-        max = int(match["max"])
-        cur = int(match["cur"])
-        return cls(user_id, listing_mongo_id=mongo_id, max=max, cur=cur)
-
-    # We want to ensure that our button is only called by the user who created it.
-    # TODO: Invert this so that the person that made it CANNOT click it
-    async def interaction_check(self, interaction: discord.Interaction) -> bool:
-        return interaction.user.id == self.user_id
-
-    async def callback(self, interaction: discord.Interaction) -> None:
-        if self.cur >= self.max:
+    async def callback(self, interaction: discord.Interaction):
+        if self.creator == interaction.user.id:
+            await interaction.response.send_message(
+                "You can't RSVP to your own listing - you're already in it, silly!",
+                ephemeral=True,
+            )
             return
 
         insert_result = rsvp_to_listing(
@@ -71,48 +40,47 @@ class RSVPButton(
         if insert_result.modified_count == 1:
             self.cur += 1
 
-        # Set button style and ID
-        self.item.style = self.style
-        self.custom_id = f"rsvp:user:{self.user_id}:mongoid:{self.mongo_id}:max:{self.max}:cur:{self.cur}"
-
         # Update embed
         embed = interaction.message.embeds[0]
-        participants = embed.fields[3].value + f"{interaction.user.mention}\n"
-        embed.set_field_at(3, name="Participants", value=participants, inline=False)
+        participants = embed.fields[4].value + f"""{interaction.user.mention}
+        {chr(173)}"""
+        embed.set_field_at(4, name="Participants", value=participants, inline=False)
+
+        if self.cur >= self.max:
+            self.view.remove_item(self)
 
         await interaction.response.edit_message(embed=embed, view=self.view)
 
 
-class BackupButton(
-    discord.ui.DynamicItem[discord.ui.Button], template=r"backup:user:(?P<id>[0-9]+)"
-):
-    def __init__(self, user_id: int, listing_mongo_id: str) -> None:
-        self.user_id: int = user_id
+class BackupButton(Button):
+    def __init__(self, listing_mongo_id: str, creator: int):
         self.mongo_id = listing_mongo_id
+        self.creator = creator
         super().__init__(
-            discord.ui.Button(
-                label="I'll be a backup!",
-                style=self.style,
-                custom_id=f"backup:user:{user_id}",
-                emoji="\N{WARNING SIGN}",
-            )
+            label="I'll be a Backup",
+            style=discord.ButtonStyle.grey,
+            emoji="\N{WARNING SIGN}",
         )
 
-    @property
-    def style(self) -> discord.ButtonStyle:
-        return discord.ButtonStyle.grey
+    async def callback(self, interaction: discord.Interaction):
+        if self.creator == interaction.user.id:
+            await interaction.response.send_message(
+                "You can't RSVP to your own listing - you're already in it, silly!",
+                ephemeral=True,
+            )
+            return
 
-    # This method actually extracts the information from the custom ID and creates the item.
-    # @classmethod
-    # async def from_custom_id(cls, interaction: discord.Interaction, item: discord.ui.Button, match: re.Match[str], /):
-    #     count = int(match['count'])
-    #     user_id = int(match['id'])
-    #     return cls(user_id, count=count)
-
-    async def callback(self, interaction: discord.Interaction) -> None:
         insert_result = rsvp_as_backup(
             listing_id=self.mongo_id, user_id=interaction.user.id
         )
 
         if insert_result.modified_count == 1:
-            self.item.style = discord.ButtonStyle.green
+            self.cur += 1
+
+        # Update embed
+        embed = interaction.message.embeds[0]
+        backups = embed.fields[5].value + f"""{interaction.user.mention}
+        {chr(173)}"""
+        embed.set_field_at(5, name="Backups", value=backups, inline=False)
+
+        await interaction.response.edit_message(embed=embed, view=self.view)
